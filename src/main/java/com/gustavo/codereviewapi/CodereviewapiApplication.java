73a8ec6384933d468b829aac2645f799de7209bd
GuSalesDev <242986472+GuSalesDev@users.noreply.github.com>
2026-09-10 21:08:32 -0300

    Api de codereview

---
+++ src/main/java/com/gustavo/codereviewapi/CodereviewapiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.codereviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodereviewapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodereviewapiApplication.class, args);
+    }
+
+}
